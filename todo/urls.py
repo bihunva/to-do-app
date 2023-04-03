e1b00c829a82a2b690db164fa9c98c208965be45
Vadym Bihun <bihunva@gmail.com>
2023-04-03 12:46:17 +0300

    Added styles to index & tags pages

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from todo.views import index
+from todo.views import index, TagListView
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("tags/", TagListView.as_view(), name="tag-list")
 ]
 
 app_name = "todo"
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.views import generic
 
-from todo.models import Task
+from todo.models import Task, Tag
 
 
 def index(request):
@@ -8,3 +9,8 @@ def index(request):
     context = {"tasks": tasks}
 
     return render(request, "todo/index.html", context=context)
+
+
+class TagListView(generic.ListView):
+    model = Tag
+
